11d155fcc1d80c91b4f50bd3be54016c120d5834
Hakan Çevik <hakanxcevik7@gmail.com>
2022-08-22 17:46:28 +0300

    runnable&handler

--- app/src/main/java/com/hakancevik/runnablehandler/MainActivity.java
+++ app/src/main/java/com/hakancevik/runnablehandler/MainActivity.java
@@ -2,82 +2,89 @@ package com.hakancevik.runnablehandler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.hakancevik.runnablehandler.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     Runnable runnable;
     Handler handler;
 
     int number;
-    Button buttonStart;
-    Button buttonStop;
-    Button buttonReset;
-    TextView textView;
+    Button startButton;
+    Button stopButton;
+    Button resetButton;
+    TextView timeText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         number = 0;
-        textView = findViewById(R.id.textView);
-        buttonStart = findViewById(R.id.button);
-        buttonStop = findViewById(R.id.button2);
-        buttonReset = findViewById(R.id.button3);
+        timeText = binding.timeText;
+        startButton = binding.startButton;
+        stopButton = binding.stopButton;
+        resetButton = binding.resetButton;
 
     }
 
 
-
-    public void start(View view){
+    public void start(View view) {
 
         handler = new Handler();
         runnable = new Runnable() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void run() {
-                textView.setText("Time (sec): "+number);
-                number++;
-                textView.setText("Time (sec): "+number);
 
-                handler.postDelayed(runnable,1000); // 1 saniyede bir sayacak.
+                timeText.setText("Time (sec) : " + number);
+                number++;
+
+                handler.postDelayed(runnable, 1000); // count one second.
 
             }
         };
 
         handler.post(runnable);
-        buttonStart.setEnabled(false);
-        buttonReset.setEnabled(true);
-        buttonStop.setEnabled(true);
+        startButton.setEnabled(false);
+        resetButton.setEnabled(true);
+        stopButton.setEnabled(true);
 
     }
 
 
-    public void stop(View view){
+    public void stop(View view) {
 
-        buttonReset.setEnabled(true);
-        buttonStart.setEnabled(true);
+        resetButton.setEnabled(true);
+        startButton.setEnabled(true);
 
         handler.removeCallbacks(runnable);
-        buttonStop.setEnabled(false);
+        stopButton.setEnabled(false);
 
     }
 
 
-
-    public void reset(View view){
+    @SuppressLint("SetTextI18n")
+    public void reset(View view) {
 
         handler.removeCallbacks(runnable);
-        number=0;
-        textView.setText("Time (sec): "+number);
-        buttonReset.setEnabled(false);
-        buttonStart.setEnabled(true);
-        buttonStop.setEnabled(false);
+        number = 0;
+        timeText.setText("Time (sec) : " + number);
+        resetButton.setEnabled(false);
+        startButton.setEnabled(true);
+        stopButton.setEnabled(false);
 
     }
 
